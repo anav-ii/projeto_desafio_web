0f5ad57c8e83bde68044e59c15645d3f6f86c766
Tarde Cetafaju <Tarde.Cetafaju@labfies.org.br>
2026-06-11 16:38:15 -0300

    criado estrutura do scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,93 @@
 console.log("teste")
+
+console.log("comando de saida")
+
+//comentario de linhas simples
+
+/*
+comentario de multiplas linhas,
+ ou seja, posso descrever algo
+ neste tipo de comentario...
+*/
+
+
+//criando variáveis
+let num = 50
+var num2 = 100
+const num3 = 450
+
+console.log(num)
+console.log(num2)
+console.log(num3)
+
+if (true){
+    let num4 = 420
+
+    var num5 = 180
+
+    console.log(num4)
+    console.log(num)
+}
+
+consoge.log(num5)
+
+num - 5
+console.log(num)
+
+num2 = 6 
+console.log(num2)
+
+
+//CONCATENAÇÃO
+console.log("Valor de um" + num)
+console.log("Valor de num3" , num3)
+console.log("Valor de num2 $ {num2}")
+console.log("a soma dos numeros" + (15 - 8))
+console.log("A soma dos numeros" , 15 + 8)
+console.log("A soma dos numeros ${15 + 8}")
+console.log("A soma dos numeros" + num + num2)
+console.log("A soma dos numeros" , num2 + 8)
+console.log("A soma dos numeros ${num3 - num2}")
+
+//OPERADORES MATEMÁTICOS
+/*
+   + SOMA
+   - SUBTRAÇÃO
+   * MULTIPLICAÇÃO
+   / DIVISÃO
+   % MÓDULO (RESTO DA DIVISÃO)
+
+ */
+
+   console.log("SOMA DAS VARIÁVEIS" , num + num2) 
+   console.log("SUBTRAÇÃO DAS VARIÁVEL" , num - num) 
+   console.log("MULTIPLICAÇÃO" , num5 % 2) 
+   console.log("DIVISÃO DE VALORES" , num2 / 5) 
+   console.log("RESTO DA DIVISÃO" , num2 % 2) 
+   console.log("RESTO DA DIVISÃO ${num3 % 3}") 
+
+   //OPERADORES RELACIONAIS
+   /*
+     -- IGUAL
+     > MAIOR
+     < MENOR
+     >= MAIOR IGUAL
+     <= MENOR IGUAL
+     != DIFERENTE
+   */
+  
+//OPERADORES LÓGICOS
+/*
+  && E
+  || OU
+
+ */
+
+  console.log("TESTE DE RALACIONAIS ", 10 == 5)
+  console.log("TESTE RELACIONAIS ", 10 != 5)
+  console.log("TESTE RELACIONAIS ", 10 > 5)
+  console.log("TESTE RELACIONAIS ", 10 < 5)
+  console.log("TESTE RELACIONAIS ", 10 >= 5)
+  console.log("TESTE RELACIONAIS ", 10 <= 5)
+  console.log("TESTE LÓGICO ", (10 > 5) && (15 < 8))
+  console.log("TESTE LÓGICO ", ( 10> 5) || (15 < 8)) 
